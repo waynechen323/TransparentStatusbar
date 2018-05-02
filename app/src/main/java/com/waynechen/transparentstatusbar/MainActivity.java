6bb92ae0c6ffb4ef28e58eead67ef2b9bad2151b
Wayne Chen <35021878+waynechen323@users.noreply.github.com>
2018-05-02 11:09:53 +0800

    According to the UI style flag to change the icon color style.

--- app/src/main/java/com/waynechen/transparentstatusbar/MainActivity.java
+++ app/src/main/java/com/waynechen/transparentstatusbar/MainActivity.java
@@ -125,8 +125,11 @@ public class MainActivity extends AppCompatActivity
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // 5.0
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            );
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
